97ea7e373f3f0bd12913dc1a350fd2e520e6ddd0
Clay Dugo <claydugo@gmail.com>
2021-04-09 13:38:54 -0400

    initial commit

---
+++ hashing.groovy
@@ -0,0 +1,16 @@
+// Groovy Demo
+def stringToHash = 'Groovy - CSC434'
+
+// Simple Hashing and Mapping
+def hashes = [md5: stringToHash.md5(), md2: stringToHash.digest('MD2'), sha1: stringToHash.digest('SHA-1'), sha256: stringToHash.digest('SHA-256')] 
+
+def hashValues = [md5: 'b161b126527818e8c77a084d10ec50d2', md2: '23e07fb63153e1c0f8a1df90ea843392', sha1: '18c32ec9c335d4abca43c65c85e0bcf05937e83c', sha256: '0754fb5fc9b269342ce718dbcc548356a82a60c112b5f430128c5f5446802f56']
+
+// Assertations 
+assert hashes.md5 == 'b161b126527818e8c77a084d10ec50d2'
+
+// All Together
+for (hash in hashes.keySet()) {
+  println "$hash: ${hashes[hash]}"
+  assert hashes[hash] == hashValues[hash]
+}
