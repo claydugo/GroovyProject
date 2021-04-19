b7d270b023e6248062ecd400a59983282fc05876
Clay Dugo <claydugo@gmail.com>
2021-04-19 15:23:55 -0400

    final

--- demo.groovy
+++ demo.groovy
@@ -7,7 +7,7 @@ def hashes = [md5: stringToHash.md5(), md2: stringToHash.digest('MD2'), sha1: st
 def hashValues = [md5: 'b161b126527818e8c77a084d10ec50d2', md2: '23e07fb63153e1c0f8a1df90ea843392', sha1: '18c32ec9c335d4abca43c65c85e0bcf05937e83c', sha256: '0754fb5fc9b269342ce718dbcc548356a82a60c112b5f430128c5f5446802f56']
 
 // Assertations 
-assert hashes.md5 == 'asdasd'
+assert hashes.md5 == 'b161b126527818e8c77a084d10ec50d2'
 
 // All Together, with String interpolation
 for (hash in hashes.keySet()) {
